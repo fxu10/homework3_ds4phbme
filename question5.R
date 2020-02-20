8d5c0c8621d1c9199b2fe0bf029e93ecf529309f
fxu10 <fxu10@jhu.edu>
2020-02-20 03:53:04 +0000

    Simplified Q5

--- question5.R
+++ question5.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(magrittr)
 library(ggplot2)
 library(tidyverse)
+
 rawdat <- readLines("healthcare-spending.csv")
 rawdat <- rawdat[3:56] # Manually remove some lines of text
 dat <- read.csv(text=rawdat,header=TRUE)
@@ -14,23 +15,11 @@ states <- names(dat)
 dat<-cbind(c(1991:2014),dat)
 names(dat) <- c("Year",names(dat)[2:length(names(dat))])
 
-# For a proper states index that is stacked vertically, exclude the US
-more_states <- rep(states[2:length(states)], times=1, each=length(dat$Year))
+dat <- within(dat, rm("United States"))
 
-# For stacking data vertically
-restacked_dat <- as.data.frame(t(as.matrix(
-  rbind(dat$Year, dat[,3]))))
-
-for(i in c(4:length(names(dat)))){
-  stateDat <- as.data.frame(t(as.matrix(rbind(dat$Year,dat[,i]))))
-  restacked_dat <- rbind(restacked_dat,stateDat)
-}
-  
-restacked_dat <- cbind(more_states, restacked_dat)
-names(restacked_dat) <- c("State","Year","Spending")
+dat<-dat %>% pivot_longer(-Year, names_to = "State", values_to = "Spending")
 
 ggplot(data=restacked_dat,aes(x=Year,y=Spending,group=State)) +
   geom_line(aes(color=State)) +
   geom_point(aes(color=State)) + 
   theme(legend.position="bottom")
-
